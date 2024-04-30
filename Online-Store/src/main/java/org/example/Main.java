f4e8a83aca088e80a20dc52215ad53a5b4bfc449
Briana P <90167653+brianapadilla24@users.noreply.github.com>
2024-04-30 15:58:56 -0500

    Online Store
    Online Store

---
+++ Online-Store/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Screen.loadProducts();
+        Screen screen = new Screen();
+        screen.homeScreen();
+    }
+}
